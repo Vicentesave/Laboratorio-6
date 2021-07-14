21cd568a0e2a642cceb500325532b8781ba19b32
Vicentesve <vicente.40080@gmail.com>
2021-07-14 14:42:40 -0500

    terminado

--- Lab6/js/index.js
+++ Lab6/js/index.js
@@ -2,20 +2,33 @@ $(".agregar" ).click(function(e) {
     e.preventDefault();
     var listText = $("#newText").val();
 
+    if(listText.trim() == "") {
+        alert('Write the item first');
+        return;
+    }
+
     $(`<div class="divLista">
         <li class="lis">
 
             <p class="itemShop">
-            ${listText}<br><br>
+            ${listText}<br><br></p>
                 <button class="checar">Check</button>
                 <button class="del">Delete</button>
-            </p>
+            
             
         </li>
     
-    </div>`).appendTo(".Lista"); 
+    </div>`).appendTo(".Lista");
+
+    listText = '';
+    $("#newText").val('');
+    
 });
 
 $(".Lista").on("click", ".checar", function() {
-    $(this).parent().toggleClass("chec");
+    $( this ).parent().toggleClass("chec")  
+});
+
+$(".Lista").on("click", ".del", function() {
+    $( this ).parent().parent().remove();
 });
